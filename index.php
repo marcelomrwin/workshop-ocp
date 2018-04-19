6e13f4e4a1284eca22152d3cee8cb3c3e8aa4f35
marcelo daniel silva sales <marcelo.d.sales@gmail.com>
2018-04-19 16:41:12 -0300

    crack php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
+echo $_ENV['mysql_db'];
+echo $_ENV['mysql_user'];
+echo $_ENV['mysql_pass'];
+echo "<br><hr>";
+echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
+$conn = new mysqli("mysql", $_ENV['mysql_user'], $_ENV['mysql_pass'], $_ENV['mysql_db']);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$result = $conn->query("SELECT nome FROM cidade");
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<h3>" . $row["nome"] . "</h3>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
